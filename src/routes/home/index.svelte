e75a6052ec0ebbb7f2341f161893d70c958360df
Robert Rocker <robertcl795.rc@gmail.com>
2022-03-12 05:10:28 -0600

    feat: add main layout placeholder
     - Add Components:
       - Navigation
       - Trending
       - Icon
     - Add Home Layout

---
+++ src/routes/home/index.svelte
@@ -0,0 +1,19 @@
+<script>
+import Icon from "$root/components/icon.svelte";
+
+</script>
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+<h1>Feed</h1>
+
+<style>
+    h1 {
+        position: sticky;
+        top: 0;
+        padding: var(--spacing-8) var(--spacing-24);
+        font-size: var(--font-24);
+        backdrop-filter: blur(100px);
+    }
+</style>
