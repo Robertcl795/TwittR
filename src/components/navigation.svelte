cb161a499649003ee4ef064e442b8398c3c974ae
Robert Rocker <robertcl795.rc@gmail.com>
2022-04-26 00:49:12 -0500

    feat: cleaning up
     - add prisma endpoints in utils
     - refactor endpoint usage

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -11,25 +11,25 @@
             <ul>
                 <li><a href="/" class="logo">🐦</a></li>
                 <li class:active={path === '/home'}>
-                    <a href="/home">
+                    <a href="/home" sveltekit:prefetch>
                         <Icon width="32" height="32" name="home" />
                         <span>Home</span>
                     </a>
                 </li>
                 <li class:active={path === '/home/profile/matia'}>
-                    <a href="/home/profile/matia">
+                    <a href="/home/profile/matia" sveltekit:prefetch>
                         <Icon width="32" height="32" name="profile" />
                         <span>Profile</span>
                     </a>
                 </li>
                 <li class:active={path === '/home/settings'}>
-                    <a href="/home/settings">
+                    <a href="/home/settings" sveltekit:prefetch>
                         <Icon width="32" height="32" name="settings" />
                         <span>Settings</span>
                     </a>
                 </li>
                 <li class:active={path === '/home/about'}>
-                    <a href="/about">
+                    <a href="/about" sveltekit:prefetch>
                         <Icon width="32" height="32" name="about" />
                         <span>about</span>
                     </a>
--- src/routes/home/__layout.svelte
+++ src/routes/home/__layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
+	import { page } from '$app/stores'
 	import Navigation from '$root/components/navigation.svelte'
+import Transition from '$root/components/transition.svelte';
 	import Trending from '$root/components/trending.svelte'
 </script>
 
 <div class="container">
 	<Navigation />
 	<main class="feed">
-		<slot />
+		<Transition url={$page.url}>
+			<slot />
+		</Transition>
 	</main>
 	<Trending />
 </div>
